e800c6ae85b50f86afed6b8c63cd2912cef37fc6
Daniel Rothfusz <dpr7@rice.edu>
2021-01-03 15:40:15 -0600

    Updated for FFT conversion (fixed datatypes for export)

--- AudioIO.py
+++ AudioIO.py
@@ -1,5 +1,16 @@
 from scipy.io import wavfile
+from scipy import fft
+import numpy as np
 
-fs, sample = wavfile.read("301-project-train-noisy/sample0.wav")
+fs, sample = wavfile.read("301-project-train-noisy/sample3.wav")
 
-wavfile.write("test_outputs/sample0.wav", fs, sample)
+print(sample.shape)
+print(type(sample[0]))
+# output = sample
+output = np.real(fft.ifft(fft.fft(sample))).astype(np.int16)
+print(output.shape)
+
+# print(sample[0:50])
+# print(output[0:50])
+wavfile.write("test_outputs/there_and_back3.wav", fs, output)
+wavfile.write("test_outputs/sample3.wav", fs, sample)
