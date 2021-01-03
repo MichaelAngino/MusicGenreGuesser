a13869e4164afa7a6253699893ceca44e4339133
Michael Angino <mfa7@rice.edu>
2021-01-03 18:00:48 -0500

    Did a loop to get clean all audio

--- AudioCleanup.py
+++ AudioCleanup.py
@@ -3,25 +3,30 @@ import matplotlib.pyplot as plt
 import matplotlib.image as im
 from scipy import fft
 from scipy.io import wavfile
+import tqdm
 
-fs, sample = wavfile.read("301-project-train-noisy/sample1.wav")
 
-spectrum = fft.fftshift(fft.fft(sample))
-abs_spec = np.abs(spectrum)
-print("partway")
-plt.stem(abs_spec[80000:130000], use_line_collection=True)
-plt.show()
-fftlength = len(spectrum)
-print(len(abs_spec))
-# Notice, band from 90000 to 122000 is unusually increased. To clean, we must remove that bump
 
-zeros = np.zeros((122000 - 90000))
+for i in tqdm.tqdm(range(0, 700)):
 
-spectrum[90000:122000] = zeros
-spectrum[fftlength-122000: fftlength-90000] = zeros
 
-output = np.real(fft.ifft(fft.ifftshift(spectrum))).astype(np.int16).T
+    fs, sample = wavfile.read("301-project-train-noisy/sample"+ str(i) +".wav")
 
-wavfile.write("test_outputs/clean1.wav", fs, output)
+    spectrum = fft.fftshift(fft.fft(sample))
+    abs_spec = np.abs(spectrum)
+    # plt.stem(abs_spec[80000:130000], use_line_collection=True)
+    # plt.show()
+    fftlength = len(spectrum)
+    # print(len(abs_spec))
+    # Notice, band from 90000 to 122000 is unusually increased. To clean, we must remove that bump
+
+    zeros = np.zeros((122000 - 90000))
+
+    spectrum[90000:122000] = zeros
+    spectrum[fftlength-122000: fftlength-90000] = zeros
+
+    output = np.real(fft.ifft(fft.ifftshift(spectrum))).astype(np.int16).T
+
+    wavfile.write("test_outputs/clean" + str(i) + ".wav", fs, output)
 
 print("done")
