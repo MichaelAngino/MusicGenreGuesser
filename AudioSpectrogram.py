7f8ca9b95969f8e96eb9dfed61601cbaa86585a2
Michael Angino <mfa7@rice.edu>
2021-01-03 22:25:52 -0500

    Added spectrogram, havent finished the spectrogram matrix though

--- AudioSpectrogram.py
+++ AudioSpectrogram.py
@@ -4,11 +4,31 @@ import numpy as np
 import librosa
 import matplotlib.pyplot as plt
 import librosa.display
+import tqdm
+import json
+
+def print_spectrogram(song_number):
+    # fs, sample = wavfile.read("301-project-train-noisy/sample3.wav")
+    sample, fs = librosa.load("301-project-train-noisy/sample"+ str(song_number) +".wav")
+    X = librosa.stft(sample)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr=fs, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    plt.show()
+
+def make_spectrogram_matrix(output_address):
+    matrix = []
+    for i in tqdm.tqdm(range(0, 7)):
+        sample, fs = librosa.load("301-project-train-noisy/sample" + str(i) + ".wav")
+        matrix.append(sample)
+
+    npmatrix = np.array(matrix)
 
 
+    np.save(output_address, npmatrix, allow_pickle= True)
 
-X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
-plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.colorbar()
+
+make_spectrogram_matrix("test")
+
+print(np.load("test.npy"))
